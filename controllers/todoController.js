e36f5ee642f7d562f31b6e10209a97d3de510285
linhaishe <xusumu@gmail.com>
2019-07-16 23:26:30 +0800

    db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -6,23 +6,31 @@ var mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://hfpp2012:hfpp2012@ds151068.mlab.com:51068/todos',{ useNewUrlParser: true });
 // const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://user1-heather:lcx@!9670itsmyfreecluster-5fqa0.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
+const uri = "mongodb+srv://user1-heather:heather01@itsmyfreecluster-5fqa0.mongodb.net/todolist";
+const client = mongoose.connect(uri, { useNewUrlParser: true })
+.then(() => {
+  console.log("Connected to mongo database");
+})
+.catch((err) => {
+  console.log("Error connecting mongo database", err);
+});
+
 // client.connect(err => {
-//   const collection = client.db("todolist").collection("todo");
+//   const collection = client.db("todolist").collection("todos");
 //   // perform actions on the collection object
 //   client.close();
 // });
 
-// mongoose.connect(uri);
 
-mongoose.connect("mongodb+srv://user1-heather:heather01@itsmyfreecluster-5fqa0.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true })
-    .then(()=>{
-        console.log("Connected to mongo database");
-    })
-    .catch((err)=>{
-        console.log("Error connecting mongo database",err);
-    });
+// mongoose.connect("mongodb+srv://user1-heather:heather01@itsmyfreecluster-5fqa0.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
+//   .then(() => {
+//     console.log("Connected to mongo database");
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting mongo database", err);
+//   });
+
+
 
 
 var todoSchema = new mongoose.Schema({
@@ -40,12 +48,12 @@ var Todo = mongoose.model('Todo', todoSchema);
 //
 // var data = [ {item: 'get milk'}, {item: "walk dog"}, {item: 'kick some coding ass'} ];;
 
-module.exports = function(app) {
+module.exports = function (app) {
   //请求列表时需要的路由
-  app.get('/todo', function(req, res) {
+  app.get('/todo', function (req, res) {
     //todo指的是mongogoose.model
     //{}代表取出所有数据
-    Todo.find({}, function(err, data) {
+    Todo.find({}, function (err, data) {
       if (err) throw err;
       //渲染数据，将data响应到ejs文件中的todos中
       res.render('todo', { todos: data });
@@ -53,20 +61,20 @@ module.exports = function(app) {
   });
 
   //发送时接收的路由
-  app.post('/todo', urlencodedParser, function(req, res) {
-    var itemOne = Todo(req.body).save(function(err, data) {
+  app.post('/todo', urlencodedParser, function (req, res) {
+    var itemOne = Todo(req.body).save(function (err, data) {
       if (err) throw err;
       res.json(data);
     });
   });
 
   //删除时需要用的路由
-  app.delete('/todo/:item', function(req, res) {
+  app.delete('/todo/:item', function (req, res) {
     // data = data.filter(function(todo) {
     //   return todo.item.replace(/ /g, "-") !== req.params.item;
     // });
     //根据唯一id进行删除会更好
-    Todo.find({item: req.params.item.replace(/-/g, " ")}).remove(function(err, data) {
+    Todo.find({ item: req.params.item.replace(/-/g, " ") }).remove(function (err, data) {
       if (err) throw err;
       res.json(data);
     });
